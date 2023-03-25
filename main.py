16b6303cc8b96998ea6be114f0f8fff8558efa03
Arman Mojaver <arman.mojaver@oneflow.com>
2023-03-25 14:22:53 +0100

    main: create Entity class

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import requests
 import json
 
 
-url = f'https://data.sec.gov/submissions/CIK0000320193.json'
+class Entity(object):
+    USER = "arman@mojaver.com"
+    HEADER = {"User-Agent": USER}
 
-response = requests.get(url, headers={"User-Agent": "arman@mojaver.com"})
-data = json.loads(s=response.content.decode())
+    def __init__(self, cik):
+        self.cik = cik
+        self.url = f'https://data.sec.gov/submissions/CIK{self.cik}.json'
 
-print(data)
+        self.data = None
+
+    def get_data(self):
+        response = requests.get(self.url, headers={"User-Agent": self.USER})
+        return json.loads(s=response.content.decode())
+
+    def run(self):
+        self.data = self.get_data()
 
 
+entity = Entity(cik='0000320193')
+entity.run()
+print(entity.data)
